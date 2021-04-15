25aa9a685074c0c89fb7611974b7957a5c939597
Pawel <pawel.chorzewski1@gmail.com>
2021-04-15 13:29:27 +0200

    Zmiana wyglądu nawigacji

--- header.php
+++ header.php
@@ -12,19 +12,19 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand">REPOZYTORIUM ZDJĘĆ</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nawigacja" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="collapse navbar-collapse" id="nawigacja">
+      <ul class="navbar-nav ">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" >Dodaj zdjęcie</a>
+          <a class="nav-link" >Dodaj zdjęcia</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pobierz zdjęcie</a>
+          <a class="nav-link">Pobierz zdjęcia</a>
         </li>
+       
       </ul>
-      
     </div>
   </div>
 </nav>
