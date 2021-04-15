3b7e55e7890eb1df461958dae4f30069b5998bae
Pawel <pawel.chorzewski1@gmail.com>
2021-04-15 15:59:40 +0200

    Ostylowanie wyświetlanych zdjęć

--- middle2.php
+++ middle2.php
@@ -1,7 +1,14 @@
+<body>
+<div class="container">
+<div class="row">
 <?php
 $files = "img/";
 $images = glob($files."*.png");
 foreach($images as $image){
-    echo '<img src="'.$image.'" height=200 width=300/>';
+    echo '<div class="col-sm-5 offset-sm-1">';
+    echo '<img src="'.$image.'" height=200px width=300px/>';
+    echo '</div>';
 }
 ?>
+</div>
+</div>
