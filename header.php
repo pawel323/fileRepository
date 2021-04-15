0a3d120ce3a716aba55f82e46daa51f0c45db38c
Pawel <pawel.chorzewski1@gmail.com>
2021-04-15 12:45:12 +0200

    Dodanie nawigacji w pliku header.php

--- header.php
+++ header.php
@@ -7,5 +7,24 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="main.css">
     
-    <title>Repozytorium plików</title>
+    <title>Repozytorium zdjęć</title>
 </head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">REPOZYTORIUM ZDJĘĆ</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" >Dodaj zdjęcie</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pobierz zdjęcie</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
