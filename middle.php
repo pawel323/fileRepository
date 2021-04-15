6360550ec9aef23e6b9b5eaa2a9dd54200b07f8d
Pawel <pawel.chorzewski1@gmail.com>
2021-04-15 13:21:38 +0200

    Dodanie ostylowania formularza

--- middle.php
+++ middle.php
@@ -1,4 +1,7 @@
 <body>
+<div class="container">
+<div class="row align-items-center">
+<div class="col-sm-6 offset-sm-3">
 <form>
   <div class="mb-3">
     <h3>Wyślij zdjęcia</h3>
@@ -9,3 +12,6 @@
   
   <button type="submit" class="btn btn-secondary">Wyślij</button>
 </form>
+</div>
+</div>
+</div>
