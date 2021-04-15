8fb4df9a61e53e8da2e619fff94d8b42ddbe665f
Pawel <pawel.chorzewski1@gmail.com>
2021-04-15 14:00:45 +0200

    Wprowadzenie skryptu wysyłającego pliki na serwer

---
+++ upload.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_FILES['image'])){
+     $errors= array();
+     $file_name = $_FILES['image']['name'];
+     $file_size =$_FILES['image']['size'];
+     $file_tmp =$_FILES['image']['tmp_name']; 
+     $file_type=$_FILES['image']['type'];
+     $extensions= array("jpeg","jpg","png", "webp", "pdf"); 
+    foreach($file_name as $key => $value){ 
+         $tmp = explode('.',$_FILES['image']['name'][$key]);
+         $file_ext = strtolower(end($tmp));
+         if(in_array($file_ext,$extensions)=== false){
+             $errors[]="Rozszerzenie niedozwolone.";
+         } 
+         if($file_size[$key] > 2097152){
+             $errors[]='Rozmiar pliku nie może być większy niż 2MB.';
+         } 
+     }  
+     if(empty($errors)==true){        
+         foreach($file_name as $key => $value){ 
+             move_uploaded_file($file_tmp[$key],"img/".$file_name[$key]);
+             echo "Przesłano wybrane pliki!";
+         } 
+     }
+     else{
+     print_r($errors);
+     }
+ }
+?>
