dbc8bc58dba499dabe3682cdc323e02f6aa681d8
Pawel <pawel.chorzewski1@gmail.com>
2021-04-15 12:20:45 +0200

    Dodanie części head w pliku header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    
+    <title>Repozytorium plików</title>
+</head>
