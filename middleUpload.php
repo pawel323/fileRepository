59aa822ea2f1cb0905ab3673f458cf59a83f28c1
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 09:17:58 +0200

    Uzupełnienie zawartośći middleUpload, która była wcześniej ustalona

---
+++ middleUpload.php
@@ -0,0 +1,18 @@
+<body>
+<div class="container">
+<div class="row align-items-center">
+<div class="col-sm-6 offset-sm-3">
+<form method="POST" action="upload.php" enctype="multipart/form-data">
+  <div class="mb-3">
+    <h3>Wyślij zdjęcia</h3>
+    <h5>Dopuszczane rozszerzenie .png</h5>
+    
+    <input type="file" class="custom-file-input" name="image[]" multiple="">
+    
+  </div>
+  
+  <button type="submit" class="btn btn-secondary">Wyślij</button>
+</form>
+</div>
+</div>
+</div>
