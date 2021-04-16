f20b4f8726d5a5b94de8619d96242005ff97dd16
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 10:35:48 +0200

    Dodanie komunikatu o konieczności zalogowania

--- displayUploaded.php
+++ displayUploaded.php
@@ -1,10 +1,11 @@
 <?php
 if (file_exists('header.php')) include ('header.php');
 if(isset($_SESSION['zalogowany'])){
-  if (file_exists('middleDisplay.php')) include ('middleDisplay.php');  
-}
-else{
-    header('Location: index.php');
-}
+    if (file_exists('middleDisplay.php')) include ('middleDisplay.php');  
+  }
+  else{
+      echo '<h3 style="text-align:center">Tylko dla zalogowanych użytkowników</h3></div><br/>';
+      if (file_exists('middle.php')) include ('middle.php');
+  }
 if (file_exists('footer.php')) include ('footer.php');
 ?>
