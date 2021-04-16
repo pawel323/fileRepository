4f93d2fd063bbbbc193ff5968b4c7943feaca1de
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 10:02:47 +0200

    Zmiana pliku treści strony upload

---
+++ uploadPage.php
@@ -0,0 +1,5 @@
+<?php
+if (file_exists('header.php')) include ('header.php');
+if (file_exists('middleUpload.php')) include ('middleUpload.php');
+if (file_exists('footer.php')) include ('footer.php');
+?>
