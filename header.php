1b71e2282e28a37bd38af39efb60a7fa2c149a23
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 10:09:22 +0200

    Poprawienie nawigacji serwisu

--- header.php
+++ header.php
@@ -11,17 +11,20 @@
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand">REPOZYTORIUM ZDJĘĆ</a>
+    <a class="navbar-brand" href="index.php">REPOZYTORIUM ZDJĘĆ</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nawigacja" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="nawigacja">
       <ul class="navbar-nav ">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Dodaj zdjęcia</a>
+          <a class="nav-link" href="uploadPage.php">Dodaj zdjęcia</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="displayUploaded.php">Pobierz zdjęcia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">Zarejestruj się</a>
         </li>
        
       </ul>
