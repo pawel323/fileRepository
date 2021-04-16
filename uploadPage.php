25b5245e50b923d3bb1f5dcde2a41b14bf66a22e
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 10:06:22 +0200

    Ograniczenie wyświetlanej treści dla użytkowników zalogowanych

--- uploadPage.php
+++ uploadPage.php
@@ -1,5 +1,11 @@
 <?php
 if (file_exists('header.php')) include ('header.php');
 if (file_exists('middleUpload.php')) include ('middleUpload.php');
+if(isset($_SESSION['zalogowany'])){
+    if (file_exists('middleUpload.php')) include ('middleUpload.php');  
+  }
+  else{
+      header('Location: index.php');
+  }
 if (file_exists('footer.php')) include ('footer.php');
 ?>
