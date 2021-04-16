cc8a55f8d60eb957653eaeff230651271f49d7da
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 10:36:51 +0200

    Dodanie komunikatu o konieczności logowania

--- uploadPage.php
+++ uploadPage.php
@@ -1,11 +1,11 @@
 <?php
 if (file_exists('header.php')) include ('header.php');
-if (file_exists('middleUpload.php')) include ('middleUpload.php');
 if(isset($_SESSION['zalogowany'])){
     if (file_exists('middleUpload.php')) include ('middleUpload.php');  
   }
   else{
-      header('Location: index.php');
+      echo '<h3 style="text-align:center">Tylko dla zalogowanych użytkowników</h3></div><br/>';
+      if (file_exists('middle.php')) include ('middle.php');
   }
 if (file_exists('footer.php')) include ('footer.php');
 ?>
