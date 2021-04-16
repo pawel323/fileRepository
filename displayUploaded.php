dcf9b1b7b05480b59c8d4b10d53c7a0262237b79
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 10:04:57 +0200

    Ograniczenie wyświetlanej treści

--- displayUploaded.php
+++ displayUploaded.php
@@ -1,5 +1,10 @@
 <?php
 if (file_exists('header.php')) include ('header.php');
-if (file_exists('middleDisplay.php')) include ('middleDisplay.php');
+if(isset($_SESSION['zalogowany'])){
+  if (file_exists('middleDisplay.php')) include ('middleDisplay.php');  
+}
+else{
+    header('Location: index.php');
+}
 if (file_exists('footer.php')) include ('footer.php');
 ?>
