b1d5e45012cbe23f84d0e2ca9bfb8035259bde64
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 09:56:58 +0200

    Dodanie komunikatów wyświetlających błędy logowania

--- middle.php
+++ middle.php
@@ -8,15 +8,32 @@
     <label for="login">Login</label>
     
     <input type="text" class="custom-input" name="login"/>
-    
+    <?php
+    if (isset($_SESSION['bladLogowania'])){
+      echo $_SESSION['bladLogowania'];
+      
+    }
+    ?>
     
   </div>
   <div class="mb-3">
   <label for="pass">Hasło</label>
     <input type="password" class="custom-input" name="pass"/>
+    <?php
+    if (isset($_SESSION['bladLogowania'])){
+      echo $_SESSION['bladLogowania'];
+      unset($_SESSION['bladLogowania']);
+    }
+    ?>
   </div>
   
   <button type="submit" class="btn btn-secondary">Wyślij</button>
+  <?php
+    if (isset($_SESSION['e_baza'])){
+      echo $_SESSION['e_baza'];
+      unset($_SESSION['e_baza']);
+    }
+    ?>
 </form>
 </div>
 </div>
