0e81ef45f2dd6eca5345f0c6745236d4f78fcade
Pawel <pawel.chorzewski1@gmail.com>
2021-04-16 18:17:53 +0200

    Uzupełnienie warunku dla wysyłania plików

--- uploadPage.php
+++ uploadPage.php
@@ -2,7 +2,20 @@
 session_start();
 if (file_exists('header.php')) include ('header.php');
 if(isset($_SESSION['zalogowany'])){
+$maxSize = 0;
+$files = 'img'.'/'.$_SESSION['user'].'/';
+$images = glob($files."*.jpg");
+foreach($images as $image){
+    $maxSize += filesize($image);
+}
+$maxSize = $maxSize / 1048576;
+$maxSize = number_format($maxSize, 2, '.', '');
+if ($maxSize < 3){
   if (file_exists('middleUpload.php')) include ('middleUpload.php');  
+}
+else{
+  if (file_exists('infoLimit.php')) include ('infoLimit.php');  
+}  
 }
 else{
   echo '<h3 style="text-align:center">Tylko dla zalogowanych użytkowników</h3></div><br/>';
