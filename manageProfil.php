e2841a9748d9a897c54a82f7882885e7b1e00eaa
Pawel <pawel.chorzewski1@gmail.com>
2021-04-17 20:18:02 +0200

    Dodanie nowej strony do zmiany hasła

---
+++ manageProfil.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+if (file_exists('header.php')) include ('header.php');
+if (file_exists('middleManage.php')) include ('middleManage.php');  
+if (file_exists('footer.php')) include ('footer.php');
+?>
