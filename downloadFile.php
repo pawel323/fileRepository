6ff188ca2a06c8daae19876cd913f0465e547fa0
Pawel <pawel.chorzewski1@gmail.com>
2021-04-17 19:32:55 +0200

    Dodanie skryptu pobierającego plik

---
+++ downloadFile.php
@@ -0,0 +1,8 @@
+<?php
+$f = $_POST['pobierane'];
+header('Content-Type: application/octet-stream');
+header("Content-Transfer-Encoding: Binary"); 
+header ("Content-Disposition: attachment; filename=".$f); 
+ readfile($f);
+ header('Location: displayUploaded.php');
+?>
