d3374d64ebb69ae2ad24fc556f517de5e4dd5b4b
Pawel <pawel.chorzewski1@gmail.com>
2021-04-17 16:55:11 +0200

    Poprawienie obsługi błędów w skrypcie wysyłającym pliki na serwer

--- upload.php
+++ upload.php
@@ -1,29 +1,38 @@
 <?php
-if(isset($_FILES['image'])){
-     $errors= array();
+session_start();
+if(isset($_FILES['image']))
+{
      $file_name = $_FILES['image']['name'];
      $file_size =$_FILES['image']['size'];
      $file_tmp =$_FILES['image']['tmp_name']; 
      $file_type=$_FILES['image']['type'];
-     $extensions= array("png"); 
-    foreach($file_name as $key => $value){ 
+     $extensions= array("png", "jpeg", "jpg", "pdf", "txt"); 
+     $validate_ok = true;
+    foreach($file_name as $key => $value)
+    { 
          $tmp = explode('.',$_FILES['image']['name'][$key]);
          $file_ext = strtolower(end($tmp));
-         if(in_array($file_ext,$extensions)=== false){
-             $errors[]="Rozszerzenie niedozwolone.";
+         if(!in_array($file_ext,$extensions))
+         {
+             $validate_ok = false;
+             $_SESSION['bladRozszerzenia'] = '<div>Rozszerzenie niedozwolone</div>';
+             header('Location: uploadPage.php');
          } 
-         if($file_size[$key] > 5000000){
-             $errors[]='Rozmiar pliku nie może być większy niż 5MB.';
+         if($file_size[$key] > 5000000)
+         {
+             $validate_ok = false;
+            $_SESSION['bladRozmiaru'] = 'Rozmiar pliku nie może być większy niż 5MB. ';
+            header('Location: middle.php');
          } 
      }  
-     if(empty($errors)==true){        
-         foreach($file_name as $key => $value){ 
+     if ($validate_ok === true)
+     {        
+         foreach($file_name as $key => $value)
+         { 
              move_uploaded_file($file_tmp[$key],"img/".$_SESSION['user'].'/'.$file_name[$key]);
-             header('Location: index.php');
+             header('Location: middleRegister.php');
          } 
      }
-     else{
-     print_r($errors);
-     }
- }
+    
+}
 ?>
