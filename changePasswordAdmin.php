67c3e99db17a997df35f2c605cb116558927e03f
Pawel <pawel.chorzewski1@gmail.com>
2021-04-18 12:31:23 +0200

    Dodanie zmiany hasła przez administratora

---
+++ changePasswordAdmin.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if(isset($_POST['pass'])){
+    $haslo = $_POST['pass'];
+    $userId = $_POST['userId'];
+    $haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
+    $haslo_hash = password_hash($haslo, PASSWORD_DEFAULT);
+    require_once "connect.php";
+        $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+
+        if($polaczenie->connect_errno!=0)
+        {
+            $_SESSION['e_baza'] = "Błąd łączenia z bazą danych";
+            header('Location: administration.php');
+        }
+        else{
+            $polaczenie->query("UPDATE uzytkownicy SET haslo='$haslo_hash' WHERE id='$userId'");
+            unset($_SESSION['e_haslo']);
+            header('Location: administration.php');
+
+        }
+}
+else{
+    $_SESSION['e_haslo'] = "<div>Podaj nowe hasło</div>";
+}
+?>
