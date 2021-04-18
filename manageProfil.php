c06644191d1f7234acc66cee6fed845d0c632304
Pawel <pawel.chorzewski1@gmail.com>
2021-04-18 17:35:39 +0200

    Rozróżnienie uprawnień zalogowanego użytkownika

--- manageProfil.php
+++ manageProfil.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
-if (file_exists('header.php')) include ('header.php');
+if($_SESSION['uprawnienia']==0){
+    if (file_exists('adminNav.php')) include ('adminNav.php'); 
+}
+else{
+    if (file_exists('header.php')) include ('header.php'); 
+}
 if (file_exists('middleManage.php')) include ('middleManage.php');  
 if (file_exists('footer.php')) include ('footer.php');
 ?>
