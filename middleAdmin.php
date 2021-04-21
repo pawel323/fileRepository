d4bb1a69195039da84137cef675542ef9b5391f6
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 11:47:56 +0200

    Poprawa czytelności

--- middleAdmin.php
+++ middleAdmin.php
@@ -3,7 +3,7 @@
         <div class="row">
         
             <div class="col-sm-6 offset-sm-3">
-            <h4 style="text-align:center">Zarejestrowani użytkownicy</h4>
+            
             <?php
             if(isset($_SESSION['e_haslo'])){
                 echo $_SESSION['e_haslo'];
@@ -13,7 +13,10 @@
                 echo $_SESSION['e_baza'];
                 unset($_SESSION['e_baza']);
             }
-   
+            ?>
+            <h4 style="text-align:center">Zarejestrowani użytkownicy</h4>
+
+            <?php
             require_once "connect.php";
             $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
             if ($polaczenie->connect_errno!=0)
@@ -29,24 +32,28 @@
                     $userLogin = $row["login"];
                     $userPass = $row["haslo"];
                     
-                    echo '<form method="POST" action="changePasswordAdmin.php">';
-                    echo '<input type="hidden" value="'.$userId.'" name="userId"/>';
-                    echo '<label for="login">Login</label>';
-                    echo '<input type="text" class="form-control" name="login" value="'.$userLogin.'"disabled>';
-                    echo '<label for="pass">Hasło</label>';
-                    echo '<input type="password" class="form-control" name="pass">';
-                    echo '<button type="submit" class="btn btn-secondary" style="margin-top:5px;margin-bottom:5px">Zmień hasło</button>';
-                    echo '</form>';
-                    echo '<form method="POST" action="deleteUser.php">';
-                    echo '<input type="hidden" value="'.$userId.'" name="userId"/>';
-                    echo '<input type="hidden" value="'.$userLogin.'" name="userLogin"/>';
-                    echo '<button type="submit" class="btn btn-secondary" style="margin-top:5px;margin-bottom:5px">Usuń użytkownika</button>';
-                    echo '</form>';
-                    echo '<form method="POST" action="wyswietlaniePlikowAdmin.php">';
-                    echo '<input type="hidden" value="'.$userLogin.'" name="userLogin"/>';
-                    echo '<button type="submit" class="btn btn-secondary" style="margin-top:5px;margin-bottom:5px">Pokaż folder</button>';
-                    echo '</form>';
-                    echo '<hr style="height: 10px; background: black; border: 0px;">';
+echo <<< END
+
+<form method="POST" action="changePasswordAdmin.php">
+<input type="hidden" value="$userId" name="userId"/>
+<label for="login">Login</label>
+<input type="text" class="form-control" name="login" value="$userLogin"disabled>
+<label for="pass">Hasło</label>
+<input type="password" class="form-control" name="pass">
+<button type="submit" class="btn btn-secondary" style="margin-top:5px;margin-bottom:5px">Zmień hasło</button>
+</form>
+<form method="POST" action="deleteUser.php">
+<input type="hidden" value="$userId" name="userId"/>
+<input type="hidden" value="$userLogin" name="userLogin"/>
+<button type="submit" class="btn btn-secondary" style="margin-top:5px;margin-bottom:5px">Usuń użytkownika</button>
+</form>
+<form method="POST" action="wyswietlaniePlikowAdmin.php">
+<input type="hidden" value="$userLogin" name="userLogin"/>
+<button type="submit" class="btn btn-secondary" style="margin-top:5px;margin-bottom:5px">Pokaż folder</button>
+</form>
+<hr style="height: 10px; background: black; border: 0px;">
+
+END;
                     }
                 }
                 else{
