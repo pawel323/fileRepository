edf6b4ee5d3661e96cf7632448f13bcfa2c1877a
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 11:45:15 +0200

    Dodanie strony z alertem podczas instalacji

---
+++ alert.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    
+    <title>Repozytorium plików</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">REPOZYTORIUM</a>
+    
+    </div>
+  </div>
+</nav>
+<div class="container">
+<div class="row align-items-center">
+<div class="col-sm-6 offset-sm-3">
+<h3 style="text-align:center">Coś poszło nie tak, rozpocznij zainstaluj aplikację jeszcze raz</h3>
+</div>
+</div>
+</div>
+</body>
+</html>
